d4d2812b6524d5b67446f3e4e41d92e67c70a96c
Piero <piero.hierro1@gmail.com>
2024-09-07 22:57:23 +0200

    Create graph.py

---
+++ esco/graph.py
@@ -0,0 +1,63 @@
+from neo4j import GraphDatabase
+from esco import gather_occupations
+
+URI = 'db_uri'
+AUTH = ('neo4j', 'password')
+URI_ESCO_GROUP = 'http://data.europa.eu/esco/isco/C25'
+
+
+def add_occupation(driver, occupation):
+    driver.execute_query(
+        '''
+        MERGE (n:Occupation {description: $description, title: $title, uri: $uri})
+        RETURN n
+        ''',
+        description=occupation.description, title=occupation.title, uri=occupation.uri, database_="neo4j"
+    )
+
+
+def add_skill(driver, skill):
+    driver.execute_query(
+        '''
+        MERGE (n:Skill {skillType: $skill_type, title: $title, uri: $uri})
+        RETURN n
+        ''',
+        skill_type=skill.skill_type, title=skill.title, uri=skill.uri, database_="neo4j"
+    )
+
+
+def link_occupation_skill(driver, occupation_uri, skill_uri, relation_type):
+    driver.execute_query(
+        '''
+        MATCH (node1:Occupation {uri: $occupationUri})
+        MATCH (node2:Skill {uri: $skillUri})
+        MERGE (node1)-[r:requiresSkill {relationType: $relationType}]->(node2)
+        RETURN r
+        ''',
+        occupationUri=occupation_uri, skillUri=skill_uri, relationType=relation_type, database_="neo4j"
+    )
+
+
+def process_occupation(driver, occupation):
+    # Add the occupation node
+    add_occupation(driver, occupation)
+
+    # Add essential skills and create relationships
+    for skill in occupation.skills:
+        add_skill(driver, skill)
+        link_occupation_skill(driver, occupation.uri, skill.uri, 'essential')
+
+    # Add optional skills and create relationships
+    for skill in occupation.optional_skills:
+        add_skill(driver, skill)
+        link_occupation_skill(driver, occupation.uri, skill.uri, 'optional')
+
+
+with GraphDatabase.driver(URI, auth=AUTH) as d:
+    try:
+        occupations = gather_occupations(URI_ESCO_GROUP)
+        for o in occupations:
+            process_occupation(d, o)
+        print('data added successfully')
+    except Exception as e:
+        print(f"An error occurred: {e}")
