b38825365ab4d7510d32463eeed00d1d376dfa0a
Piermuz7 <piermichele.rosati@gmail.com>
2024-10-10 15:28:43 +0200

    [edit] removed space from name columns, add_module, add_learning_outcome

--- esco/graph.py
+++ esco/graph.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+import pandas as pd
 from esco import gather_occupations
 
 URI_NEO4J = 'URI'
@@ -53,11 +54,36 @@ def process_occupation(driver, occupation):
         link_occupation_skill(driver, occupation.uri, skill.uri, 'optional')
 
 
+def add_module(driver, individual_name, course_link, course_title, course_description, course_type, course_comment,
+               course_competency_to_be_achieved, course_content):
+    driver.execute_query(
+        '''
+        MERGE (n:Module {individual_name: $individual_name, course_link: $course_link, course_title: $course_title, course_description: $course_description, course_type: $course_type, course_comment: $course_comment, course_competency_to_be_achieved: $course_competency_to_be_achieved, course_content: $course_content})
+        RETURN n
+        ''',
+        individual_name=individual_name, course_link=course_link, course_title=course_title,
+        course_description=course_description, course_type=course_type, course_comment=course_comment,
+        course_competency_to_be_achieved=course_competency_to_be_achieved, course_content=course_content,
+        database_="neo4j"
+    )
+
+def add_learning_outcome(driver, learning_outcome):
+    driver.execute_query(
+        '''
+        MERGE (n:LearningOutcome {learning_outcome: $learning_outcome})
+        RETURN n
+        ''',
+        learning_outcome=learning_outcome, database_="neo4j"
+    )
+
 with GraphDatabase.driver(URI_NEO4J, auth=AUTH) as d:
-    try:
-        occupations = gather_occupations(URI_ESCO_GROUP)
-        for o in occupations:
-            process_occupation(d, o)
-        print('data added successfully')
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # read modules.csv and ignore Nan values
+    modules_df = pd.read_csv('./csv/modules.csv').fillna('')
+    for _, module_row in modules_df.iterrows():
+        add_module(d, module_row['Individual Name'], module_row['Course_Link'], module_row['Course_Title'],
+                   module_row['Course_Description'], module_row['Course_Type'], module_row['Course_Comment'],
+                   module_row['Course_Competency_to_be_achieved'], module_row['Course_Content'])
+    # read learning_outcomes.csv and ignore Nan values
+    learning_outcomes_df = pd.read_csv('./csv/learning_outcomes.csv').fillna('')
+    for _, learning_outcome_row in learning_outcomes_df.iterrows():
+        add_learning_outcome(d, learning_outcome_row['Learning Outcome'])
