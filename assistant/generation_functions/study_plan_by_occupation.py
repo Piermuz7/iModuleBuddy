93ef1eedf27f46701bab613d0a9357d0d20dcf83
Daniele Porumboiu <30881307+rasphlat@users.noreply.github.com>
2025-01-08 18:04:09 +0100

    Mandatory modules included in the recommandation

--- assistant/generation_functions/study_plan_by_occupation.py
+++ assistant/generation_functions/study_plan_by_occupation.py
@@ -27,19 +27,24 @@ def create_study_plan_by_occupations():
         return 'No relevant modules found for the given occupation.'
     with GraphDatabase.driver(st.secrets['NEO4J_URI'], auth=auth) as driver:
         occupation_query = f"""
-        MATCH(o:Occupation)-[:requires_skill]->(s:Skill)
+        MATCH (o:Occupation)-[:requires_skill]->(s:Skill)
         WHERE o.occupation IN {occupations}
         CALL db.index.vector.queryNodes('learningOutcomeIndex', 3, s.embeddingDescription)
         YIELD node AS lo, score
         WHERE score>0.92
-        MATCH(lo)<-[:has_learning_outcome]-(m:Module)-[:has_schedule]->(ts:TeachingSession)
-        RETURN m.module_title as module, collect(DISTINCT lo.learning_outcome) as supporting_learning_outcomes, collect(distinct s{{.title,.description}}) as supported_skills, o{{.occupation,.description}} as occupation, collect(distinct ts{{.ay,.day,.group_name,.location,.periodicity,.semester,.time}}) as teaching_session
+        MATCH (lo)<-[:has_learning_outcome]-(m:Module)-[:has_schedule]->(ts:TeachingSession)
+        WHERE NOT m.module_title IN {taken_modules}
+        RETURN m{{.module_title, .module_type}} as module, collect (DISTINCT lo.learning_outcome) as supporting_learning_outcomes, collect(distinct s{{.title, .description}}) as supported_skills, o{{.occupation, .description}} as occupation, collect (distinct ts{{.ay, .day, .group_name, .location, .periodicity, .semester, .time}}) as teaching_session
         ORDER BY size(supported_skills) DESC
+        UNION
+        MATCH (m:Module{{module_type: "mandatory"}})-[:has_schedule]->(ts:TeachingSession)
+        WHERE NOT m.module_title IN {taken_modules}
+        RETURN m{{.module_title, .module_type}} as module, 'Not Given' as supporting_learning_outcomes,'Not Given' as supported_skills,'Not Given' as occupation, collect (distinct ts{{.ay, .day, .group_name, .location, .periodicity, .semester, .time}}) as teaching_session
         """
         skill_results = driver.execute_query(occupation_query)
         # Step 4: Format the modules summary for the LLM
         modules_summary = "\n".join([
-            f"- Module: {m[0]}\n Teaching Session: {m[3]}\n Occupation: {m[3]}\n  Support Learning Outcomes: {m[1]}\n  Supported Skills: {m[2]}\n Number of Supported Skills: {len(m[2])}"
+            f"- Module: {m[0]}\n Teaching Session: {m[4]}\n Occupation: {m[3]}\n  Support Learning Outcomes: {m[1]}\n  Supported Skills: {m[2]}"
             for m in skill_results.records
         ])
         # Step 5: Use the LLM to interpret the results
